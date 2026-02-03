0e2ea7f742c89aa0e8a832dfc3ab19fecc3d80af
kevmechkael <kevinmikael30@gmail.com>
2026-02-03 15:51:14 +0700

    commit ke 2

--- src/oop_111440_KevinMikael_week01/GradingSystem.kt
+++ src/oop_111440_KevinMikael_week01/GradingSystem.kt
@@ -1,8 +1,9 @@
 package oop_111440_KevinMikael_week01
 
 fun main(){
-    var name: String = "John Thor"
-    var score: Int = 80
+    val name: String = "John Thor"
+    val score: Int = 80
 
     println("nama = $name, score = $score")
+    print("Tugas Kevin")
 }
