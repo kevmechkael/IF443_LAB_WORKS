5300f4d4709963730c85d633fc8dfd9eaca4f146
kevmechkael <kevinmikael30@gmail.com>
2026-02-04 10:50:40 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_111440_KevinMikael_week01/GameStore.kt
+++ src/oop_111440_KevinMikael_week01/GameStore.kt
@@ -3,7 +3,8 @@ package oop_111440_KevinMikael_week01
 fun main(){
     val gameTitle: String = "god of war"
     val price: Int = 100000
-
-
+    var discountedPrice = discount(price)
+    println(discountedPrice)
 }
 
+fun discount(price: Int) = if (price > 500000) (price * 80 / 100).toInt() else (price * 90/100).toInt()
