fa95bf6c78792d015a8cf3a84dfdebb72f80efb4
kevmechkael <kevinmikael30@gmail.com>
2026-02-04 10:57:18 +0700

    week01: (tugas 2) implement receipt printing

--- src/oop_111440_KevinMikael_week01/GameStore.kt
+++ src/oop_111440_KevinMikael_week01/GameStore.kt
@@ -4,7 +4,12 @@ fun main(){
     val gameTitle: String = "god of war"
     val price: Int = 100000
     var discountedPrice = discount(price)
-    println(discountedPrice)
+
+    printReceipt(gameTitle, discountedPrice)
 }
 
 fun discount(price: Int) = if (price > 500000) (price * 80 / 100).toInt() else (price * 90/100).toInt()
+
+fun printReceipt(gameTitle: String, finalPrice : Int){
+    println("$gameTitle - $finalPrice ")
+}
