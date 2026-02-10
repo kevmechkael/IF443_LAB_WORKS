e8f768b32865664b6e4661206b4c668829d39ca1
kevin <kevinmikael30@gmail.com>
2026-02-10 16:06:26 +0700

    save

---
+++ src/oop_111440_KevinMikael_week02/Main.kt
@@ -0,0 +1,18 @@
+package oop_111440_KevinMikael_week02
+
+import java.util.Scanner
+
+fun main(){
+    val scanner = Scanner(System.`in`)
+    println("--- APLIKASI PMB UMN")
+    print("masukan nama:")
+    val name = scanner.nextline()
+    print("masukan NIM (wajib 5 karakter):")
+    val nim = scanner.next()
+    scanner.nextLine()
+    if(nim.length != 5){
+        println("error")
+    } else {
+        print("masukan jurusan")
+    }
+}
