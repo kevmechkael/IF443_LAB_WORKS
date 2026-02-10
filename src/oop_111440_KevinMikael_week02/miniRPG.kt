f60d17eb546eb4b44d19cdca46125a6c926bb49f
kevmechkael <kevinmikael30@gmail.com>
2026-02-10 22:35:40 +0700

    week02: (task 2) implement the game

--- src/oop_111440_KevinMikael_week02/miniRPG.kt
+++ src/oop_111440_KevinMikael_week02/miniRPG.kt
@@ -1,9 +1,9 @@
 package oop_111440_KevinMikael_week02
-
-class hero(
+import java.util.Scanner
+class Hero(
     var name: String,
-    var hp : Int = 100,
-    var baseDamage :Int
+    var baseDamage :Int,
+    var hp : Int = 100
 ){
     fun attack(targetName : String){
         println("$name menebas $targetName!")
@@ -14,13 +14,44 @@ class hero(
             hp = 0
         }
         println("remaining hp : $hp")
-        isalive(hp)
     }
-    fun isalive(hp : Int) : Boolean{
-        if (hp === 0){
-            return true
+    fun isalive() : Boolean{
+        return hp > 0
+    }
+}
+fun main(){
+    val scanner = Scanner(System.`in`)
+    var hpMusuh: Int = 100
+    print("masukan nama hero : ")
+    val nama: String = scanner.nextLine()
+    print("masukan damage hero : ")
+    val damage = scanner.nextInt()
+    val game = Hero(nama, damage)
+    while(game.isalive()){
+        println("serang(1) atau kabur (2)")
+        val choice = scanner.nextInt()
+        if (choice == 1){
+            game.attack("zombie")
+            hpMusuh -= damage
+            println("hp zombie = $hpMusuh")
+            if (hpMusuh <= 0){
+                println("kau menang")
+                break
+            }
+            game.takeDamage((10..20).random())
+        }else if(choice==2){
+            break
         }else{
-            return false
+            println("salah input tetap terkena damage")
+            game.takeDamage((10..20).random())
         }
     }
+
+    if (game.hp > hpMusuh){
+        print("kamu menang")
+    }else if(game.hp < hpMusuh){
+        print("kamu kalah")
+    }else(
+        print("seri")
+    )
 }
