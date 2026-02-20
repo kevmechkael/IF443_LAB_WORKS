35a5b899f5d24adad4d69929f902f8e83a959131
kevin <kevinmikael30@gmail.com>
2026-02-20 15:27:01 +0700

    "week03: (task 1) implement Weapon testing in main"

--- src/oop_111440_KevinMikael_week03/main.kt
+++ src/oop_111440_KevinMikael_week03/main.kt
@@ -1,9 +1,13 @@
 package oop_111440_KevinMikael_week03
 fun main(){
-    val e = Employee("budi")
-    e.salary = -1000
-    e.salary = 5000
-    println("gaji = " + e.salary)
-    e.increasePerfomanceRating()
-    println("pajaknya = " + e.tax)
+//    val e = Employee("budi")
+//    e.salary = -1000
+//    e.salary = 5000
+//    println("gaji = " + e.salary)
+//    e.increasePerfomanceRating()
+//    println("pajaknya = " + e.tax)
+    val e = tierWeapon()
+    e.damage = -50
+    e.damage = 9999
+    println(e.tier)
 }
