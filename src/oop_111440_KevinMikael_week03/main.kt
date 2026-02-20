805f4cdc54e3203983418e2f18e0dd133fe4895b
kevin <kevinmikael30@gmail.com>
2026-02-20 15:46:28 +0700

    "week03: (task 2) implement Player testing in main"

--- src/oop_111440_KevinMikael_week03/main.kt
+++ src/oop_111440_KevinMikael_week03/main.kt
@@ -6,8 +6,13 @@ fun main(){
 //    println("gaji = " + e.salary)
 //    e.increasePerfomanceRating()
 //    println("pajaknya = " + e.tax)
-    val e = tierWeapon()
-    e.damage = -50
-    e.damage = 9999
-    println(e.tier)
+//    val e = tierWeapon()
+//    e.damage = -50
+//    e.damage = 9999
+//    println(e.tier)
+    val e = PlayerLevel()
+//    e.xp = 100
+    e.addXP(50)
+    e.addXP(60)
+
 }
