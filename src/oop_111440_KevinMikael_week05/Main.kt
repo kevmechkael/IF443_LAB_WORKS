0359ceea7ddff9adc90a06e47b4c5ce7178b3eb1
kevmechkael <kevinmikael30@gmail.com>
2026-03-04 21:59:48 +0700

    week05: (task 1) test MathHelper overloading in main

--- src/oop_111440_KevinMikael_week05/Main.kt
+++ src/oop_111440_KevinMikael_week05/Main.kt
@@ -1,19 +1,24 @@
 package oop_111440_KevinMikael_week05
 
 fun main(){
-    val dosen1 = Dosen("pak dimas", "abc123")
-    val admin1 = admin("pak dimsum")
-    val daftarPegawai: List<Pegawai> = listOf(dosen1, admin1)
-    println("AKTIVITAS PEGAWAI")
-    for (pegawai in daftarPegawai) {
-        pegawai.bekerja()
-        when(pegawai){
-            is Dosen -> {
-                println("terdeteksi sebagai doesen (nidn: ${pegawai.nidn})")
-            }
-            is admin -> {
-                println("terdeteksi sebagai admin")
-            }
-        }
-    }
+//    val dosen1 = Dosen("pak dimas", "abc123")
+//    val admin1 = admin("pak dimsum")
+//    val daftarPegawai: List<Pegawai> = listOf(dosen1, admin1)
+//    println("AKTIVITAS PEGAWAI")
+//    for (pegawai in daftarPegawai) {
+//        pegawai.bekerja()
+//        when(pegawai){
+//            is Dosen -> {
+//                println("terdeteksi sebagai doesen (nidn: ${pegawai.nidn})")
+//            }
+//            is admin -> {
+//                println("terdeteksi sebagai admin")
+//            }
+//        }
+    val luasKotak = hitungLuas(28)
+    val luasPersegiPanjang = hitungLuas(50, 20)
+    val luasBulat = hitungLuas(50.1)
+    println(luasKotak)
+    println(luasPersegiPanjang)
+    println(luasBulat)
 }
