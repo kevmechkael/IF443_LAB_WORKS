8aaf663493d15fca564a2b5bef9ef1aa7886fe48
kevmechkael <kevinmikael30@gmail.com>
2026-03-04 22:11:00 +0700

    week05: (task 2) create EWallet class

---
+++ src/oop_111440_KevinMikael_week05/Ewallet.kt
@@ -0,0 +1,15 @@
+package oop_111440_KevinMikael_week05
+
+class Ewallet(accountName: String, var balance: Double):PaymentMethod(accountName) {
+    override fun processPayment(amount: Double) {
+        if (amount < balance) {
+            balance -= amount
+            println("sukses")
+        } else {
+            println("saldo tak cukup")
+        }
+    }
+    fun topup(amount: Double) {
+        balance += amount
+    }
+}
