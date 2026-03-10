d181f2f49177584ae987a48b792514c39139cdb0
kevmechkael <kevinmikael30@gmail.com>
2026-03-10 15:54:02 +0700

    smartSpeaker fix

--- src/oop_111440_KevinMikael_week06/SmartSpeaker.kt
+++ src/oop_111440_KevinMikael_week06/SmartSpeaker.kt
@@ -1,10 +1,18 @@
 package oop_111440_KevinMikael_week06
 
 class SmartSpeaker(override val id: String, override val name: String): SmartDevice, Switchable  {
-    override fun turnOn(song :String) {
-        println("speaker turning on, playing $song")
+    var status :Boolean = false
+    override fun turnOn() {
+        println("speaker turning on")
+        status = true
     }
     override fun turnOff() {
         println("speaker turning off")
+        status = false
+    }
+    fun play(song :String){
+        if (status){
+            println("playing $song")
+        }
     }
 }
