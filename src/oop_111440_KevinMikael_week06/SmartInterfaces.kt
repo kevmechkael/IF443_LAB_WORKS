a3abce2850e6886bb801ea6b6a218f2f31461c4a
kevmechkael <kevinmikael30@gmail.com>
2026-03-10 15:34:28 +0700

    week06: (task) create smart home interfaces

---
+++ src/oop_111440_KevinMikael_week06/SmartInterfaces.kt
@@ -0,0 +1,16 @@
+package oop_111440_KevinMikael_week06
+
+interface SmartDevice {
+    val id: String
+    val name: String
+}
+interface Switchable {
+    fun turnOn()
+    fun turnOff()
+}
+interface Recordable {
+    fun startRecording()
+    fun stopRecording(){
+        println("Recording stopped")
+    }
+}
