55d681fa2db8c61b93e26d6aa7fd84130e5d4884
kevmechkael <kevinmikael30@gmail.com>
2026-03-10 15:17:23 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_111440_KevinMikael_week06/GadgetInterface.kt
@@ -0,0 +1,12 @@
+package oop_111440_KevinMikael_week06
+
+interface Camera {
+    fun turnOn() {
+        println("Camera on")
+    }
+}
+interface Phone {
+    fun turnOn(){
+        println("Phone on")
+    }
+}
