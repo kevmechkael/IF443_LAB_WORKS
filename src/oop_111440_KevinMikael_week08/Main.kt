559cb824c3d24d46c0e9e2e0be309d9ed02dd658
kevmechkael <kevinmikael30@gmail.com>
2026-04-14 15:02:05 +0700

    week08: implement chained safe calls and elvis

--- src/oop_111440_KevinMikael_week08/Main.kt
+++ src/oop_111440_KevinMikael_week08/Main.kt
@@ -1,5 +1,7 @@
 package oop_111440_KevinMikael_week08
 
 fun main(){
-
+    val emptyOrder = Order(null, null)
+    val destination = emptyOrder.deliveryDetails?.address?.city?.name ?: "kota Tidak Diketahui"
+    println("tujuan ke "+destination)
 }
