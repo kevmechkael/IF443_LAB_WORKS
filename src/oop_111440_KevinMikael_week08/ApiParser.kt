bca58f8c5571a87f590a878633874fec777a3fdf
kevmechkael <kevinmikael30@gmail.com>
2026-04-14 16:27:43 +0700

    week08: (task) implement payment processing with !!

--- src/oop_111440_KevinMikael_week08/ApiParser.kt
+++ src/oop_111440_KevinMikael_week08/ApiParser.kt
@@ -1,2 +1,23 @@
 package oop_111440_KevinMikael_week08
 
+class ApiParser {
+    fun parseProduct(rawJson: Map<String, Any?>): Product? {
+        val id = requireNotNull(rawJson["id"] as? String) {"API INVALID MISSING ID"}
+        val name = requireNotNull(rawJson["name"] as? String) {"API INVALID MISSING NAME"}
+        val type = rawJson["type"] as? String
+        return if (type == "ELECTRONIC"){
+            val warranty = rawJson["warranty"] as? Int ?: 12
+            Electronic(id, name, warranty)
+        }else if(type == "CLOTHING"){
+            val size = rawJson["size"] as? String ?: "All suze"
+            Clothing(id, name, size)
+        } else null
+    }
+    fun checkout(product: Product) {
+        val ID = when (product) {
+            is Electronic -> product.id
+            is Clothing -> product.id
+        }
+        val transactionId: String = javaPaymentService.processPayment(ID)!!
+    }
+}
