9c5785452bbdffc8326c9f4cec50ffbb95ffe599
kevmechkael <kevinmikael30@gmail.com>
2026-04-14 16:27:29 +0700

    week08: (task) create JavaPaymentService for payment interop

--- src/oop_111440_KevinMikael_week08/ApiParser.kt
+++ src/oop_111440_KevinMikael_week08/ApiParser.kt
@@ -1,16 +1,2 @@
 package oop_111440_KevinMikael_week08
 
-class ApiParser {
-    fun parseProduct(rawJson: Map<String, Any?>): Product? {
-        val id = requireNotNull(rawJson["id"] as? String) {"API INVALID MISSING ID"}
-        val name = requireNotNull(rawJson["name"] as? String) {"API INVALID MISSING NAME"}
-        val type = rawJson["type"] as? String
-        return if (type == "ELECTRONIC"){
-            val warranty = rawJson["warranty"] as? Int ?: 12
-            Electronic(id, name, warranty)
-        }else if(type == "CLOTHING"){
-            val size = rawJson["size"] as? String ?: "All suze"
-            Clothing(id, name, size)
-        } else null
-    }
-}
---
+++ src/oop_111440_KevinMikael_week08/javaPaymentService.java
@@ -0,0 +1,6 @@
+package oop_111440_KevinMikael_week08;
+public class javaPaymentService{
+    public static String processPayment(String productId) {
+        return "TRX-"+productId+"-SUCCESS";
+    }
+}
