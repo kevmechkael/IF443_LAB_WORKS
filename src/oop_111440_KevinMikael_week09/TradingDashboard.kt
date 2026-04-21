5f59d6f496d01dcf7a69b60334e8d56250989a56
kevmechkael <kevinmikael30@gmail.com>
2026-04-21 15:47:17 +0700

    week09: (task) chain sortedByDescending and map for top performers

--- src/oop_111440_KevinMikael_week09/TradingDashboard.kt
+++ src/oop_111440_KevinMikael_week09/TradingDashboard.kt
@@ -3,8 +3,8 @@ package oop_111440_KevinMikael_week09
 fun main(){
     val tradeHistory = listOf(
         TradeLog("BTCUSDT", "LONG", 10, 12.5, "OPEN"),
-        TradeLog("ETHUSDT", "SHORT", 5, -8.3, "CLOSED"),
-        TradeLog("BTCUSDT", "SHORT", 20, 5.7, "OPEN"),
+        TradeLog("ETHUSDT", "SHORT", 5, 8.3, "CLOSED"),
+        TradeLog("BTCUSDT", "SHORT", 20, 5.7, "CLOSED"),
         TradeLog("ETHUSDT", "LONG", 8, -3.2, "CLOSED"),
         TradeLog("BNBUSDT", "LONG", 15, 9.1, "OPEN"),
         TradeLog("SOLUSDT", "SHORT", 12, -6.8, "CLOSED")
@@ -12,4 +12,6 @@ fun main(){
     val closedTrade = tradeHistory.filter { it.status == "CLOSED" }
     val winningTrade = closedTrade.filter { it.roe > 0 }
     val losingTrade = closedTrade.filter { it.roe <= 0 }
+    val topPerformersString = winningTrade.sortedByDescending { it.roe }.map{"WIN [${it.pair} - ${it.position}] : ${it.roe}% ROE (Lev: ${it.leverage}x)"}
+    println(topPerformersString)
 }
