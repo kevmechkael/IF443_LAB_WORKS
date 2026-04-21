8230b70f17c7eea58d24d016461d76a730ec7582
kevmechkael <kevinmikael30@gmail.com>
2026-04-21 15:59:08 +0700

    week09: (task) print top performers using forEach

--- src/oop_111440_KevinMikael_week09/TradingDashboard.kt
+++ src/oop_111440_KevinMikael_week09/TradingDashboard.kt
@@ -15,5 +15,6 @@ fun main(){
     val topPerformersString = winningTrade.sortedByDescending { it.roe }.map{"WIN [${it.pair} - ${it.position}] : ${it.roe}% ROE (Lev: ${it.leverage}x)"}
     val worstPerformersString = losingTrade.sortedByDescending { it.roe }.map{"WIN [${it.pair} - ${it.position}] : ${it.roe}% ROE (Lev: ${it.leverage}x)"}
     val uniquePairs = tradeHistory.map{ it.pair.toSet() }
-    println(uniquePairs)
+    println("=== CRyPTO TRADING DASHBOARD ===")
+    topPerformersString.forEach{println(it)}
 }
