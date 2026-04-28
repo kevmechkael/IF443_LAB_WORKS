3d703e19b2ecd361a62836a9eabf20d8a56e9d38
kevmechkael <kevinmikael30@gmail.com>
2026-04-28 16:16:48 +0700

    week10: (task) initialize coin repository

---
+++ src/oop_111440_KevinMikael_week10/CryptoDashboard.kt
@@ -0,0 +1,5 @@
+package oop_111440_KevinMikael_week10
+
+fun main(){
+    val coinRepo = WalletRepo<Coin>()
+}
--- src/oop_111440_KevinMikael_week10/WalletModels.kt
+++ src/oop_111440_KevinMikael_week10/WalletModels.kt
@@ -1,4 +1,4 @@
 package oop_111440_KevinMikael_week10
 
-data class Coin(val Name: String, val Balance: Double)
+data class Coin(override val name: String, val Balance: Double) : Name
 data class Transaction(val id: String, val amount: Double)
