7a634c66e1b382aaf8becc224b1b0daf5f13504a
kevmechkael <kevinmikael30@gmail.com>
2026-05-12 15:14:22 +0700

    week12: implement throw keyword for validation

--- src/oop_111440_KevinMikael_week12/customExceptions.kt
+++ src/oop_111440_KevinMikael_week12/customExceptions.kt
@@ -1,3 +1,17 @@
 package oop_111440_KevinMikael_week12
 
 class InsufficientFundsException(message: String) : Exception(message)
+class BankAccount(private var balance: Double) {
+
+    fun withdraw(amount: Double) {
+
+        if (amount > balance) {
+            throw InsufficientFundsException(
+                "Saldo tidak cukup! Saldo saat ini: $balance"
+            )
+        }
+
+        balance -= amount
+        println("Berhasil tarik uang Rp$amount")
+    }
+}
