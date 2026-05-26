6cf03c856c92f171a2374a5a2ffb31575bdde511
kevmechkael <kevinmikael30@gmail.com>
2026-05-26 12:59:42 +0700

    week13: implement basic writeText function

--- src/oop_111440_KevinMikael_week14/SrpViolation.kt
+++ src/oop_111440_KevinMikael_week14/SrpViolation.kt
@@ -1,2 +1,15 @@
 package oop_111440_KevinMikael_week14
 
+data class User(val name: String, val email :String, val age :Int)
+
+class UserManager{
+    fun validateUser(user: User): Boolean{
+        return user.email.contains("@") && user.age >= 18
+    }
+    fun saveUserToDatabase(user: User){
+        println("Insert INTO users VALUES ('${user.name}', '${user.email}')")
+    }
+    fun sendWelcomeEmail(user: User){
+        println("Sending Welcome email to ${user.email}")
+    }
+}
