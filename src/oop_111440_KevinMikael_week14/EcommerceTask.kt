45414cad6d22c13440ad219c15678cf42e89c08f
kevmechkael <kevinmikael30@gmail.com>
2026-05-26 15:50:13 +0700

    week14: (task) refactor OrderProcessor applying SRP and DIP

--- src/oop_111440_KevinMikael_week14/EcommerceTask.kt
+++ src/oop_111440_KevinMikael_week14/EcommerceTask.kt
@@ -1,15 +1,50 @@
 package oop_111440_KevinMikael_week14
+import oop_111440_KevinMikael_week08.Order
 import java.io.File
-class BadOrderProcessor{
-    private val file = File("orders.csv")
-    fun processOrder(itemName: String, basePrice: Double, customerType: String){
-        val finalPrice = when (customerType){
+import java.io.FileWriter
+
+//class BadOrderProcessor{
+//    private val file = File("orders.csv")
+//    fun processOrder(itemName: String, basePrice: Double, customerType: String){
+//        val finalPrice = when (customerType){
+//            "REGULAR" -> basePrice
+//            "VIP" -> basePrice * 0.9
+//            else -> basePrice
+//        }
+//        println("${itemName} $finalPrice")
+//        file.appendText("${itemName} $finalPrice\n")
+//        println("$itemName telah dikonfirmasi")
+//    }
+//}
+
+interface OrderRepository{
+    fun saveOrder(itemName: String, basePrice: Double, customerType: String)
+}
+class CsvOrderRepository (val fileName: String = "orders.csv") : OrderRepository{
+    override fun saveOrder(itemName: String, basePrice: Double, customerType: String) {
+        FileWriter(File(fileName), true).use {
+            writer -> writer.append("$itemName $basePrice $customerType\n")
+        }
+    }
+}
+
+interface NotificationService{
+    fun sendNotification(itemName : String)
+}
+
+class EmailNotifier : NotificationService{
+    override fun sendNotification(itemName : String) = println("email terkirim pesanan $itemName Anda telah dikonfirmasi")
+}
+
+class SafeOrderProcessor(val repo: OrderRepository, val notifier: NotificationService) {
+    fun processOrder(itemName: String, basePrice: Double, customerType: String) {
+        val finalPrice = when (customerType.uppercase()) {
+            "VIP" -> basePrice * 0.90
             "REGULAR" -> basePrice
-            "VIP" -> basePrice * 0.9
             else -> basePrice
         }
-        println("${itemName} $finalPrice")
-        file.appendText("${itemName} $finalPrice\n")
-        println("$itemName telah dikonfirmasi")
+        println("Memproses pesanan $itemName seharga $finalPrice")
+        repo.saveOrder(itemName, finalPrice, customerType)
+        notifier.sendNotification(itemName)
     }
 }
